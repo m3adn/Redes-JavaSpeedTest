a70364ebf34cbabd956153a1b99524f3dd136cd1
m3adn <58094715+m3adn@users.noreply.github.com>
2020-06-25 18:03:20 +0100

    Update Server.java

--- code/Server.java
+++ code/Server.java
@@ -10,13 +10,13 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args){
-        int port = 6666;
+        int port = 6000;
         try { ServerSocket server = new ServerSocket(port);// criamos o socket do servidor e associamos-no com a porta especificada
                System.out.println("Esperemos pelo cliente....");
                Socket socket = server.accept();
                System.out.println("Encontramos um :) ... Finalmente!\n");
                
-         // Pegamos no fluxo de saida e de entrado do socket. Agora jÃ¡ podemos receber e enviar dados :) ....
+         // Pegamos no fluxo de saida e de entrado do socket. Agora ja podemos receber e enviar dados :) ....
          InputStream sin = socket.getInputStream();
          OutputStream sout = socket.getOutputStream();
 
